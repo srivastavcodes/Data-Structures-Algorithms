ee54cf11cb368d73e05b3feac1857d2a0a2fbd86
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-07 14:31:18 +0530

    more simplistic

--- Step 1/src/learnbasicrecursion/PrintNameNTimes.java
+++ Step 1/src/learnbasicrecursion/PrintNameNTimes.java
@@ -6,22 +6,19 @@ public class PrintNameNTimes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your Name::");
-        String name = sc.nextLine();
-
         System.out.println("Enter the amount::");
         int times = sc.nextInt();
 
-        printNames(name, times);
+        printNames(times);
     }
 
     // This is a backtracking example.
-    private static void printNames(String name, int times) {
+    private static void printNames(int times) {
         if (times < 1) {
             return;
         }
-        printNames(name, times - 1);
-        System.out.println(name + times);
+        printNames(times - 1);
+        System.out.print("GFG" + " ");
     }
 }
 
