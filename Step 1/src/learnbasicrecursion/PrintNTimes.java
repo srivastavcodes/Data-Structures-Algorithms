f44ccdaf5f55adb332835860cbb24f847cae3329
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-07 11:25:38 +0530

    print a number N times

--- Step 1/src/learnbasicrecursion/PrintNTimes.java
+++ Step 1/src/learnbasicrecursion/PrintNTimes.java
@@ -2,6 +2,18 @@ package learnbasicrecursion;
 
 public class PrintNTimes {
     public static void main(String[] args) {
-        
+        System.out.println();
+
+        int count = 0;
+        printNumber(count);
+    }
+
+    public static void printNumber(int count) {
+        System.out.println(count);
+        if (count == 150) {
+            return;
+        }
+        count++;
+        printNumber(count);
     }
 }
