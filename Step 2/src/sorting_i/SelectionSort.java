222bb7686290a12bec7faa6ea8cd1f4b28c5ec11
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-08 20:30:09 +0530

    selection sort

--- Step 2/src/sorting_i/SelectionSort.java
+++ Step 2/src/sorting_i/SelectionSort.java
@@ -1,7 +1,32 @@
 package sorting_i;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
 
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        System.out.print(Arrays.toString(selectSort(arr)));
+    }
+
+    private static int[] selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            compareAndSwap(arr, i);
+        }
+        return arr;
+    }
+
+    private static void compareAndSwap(int[] arr, int i) {
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                swapElements(arr, i, j);
+            }
+        }
+    }
+
+    private static void swapElements(int[] arr, int left, int right) {
+        int hv = arr[left];
+        arr[left] = arr[right];
+        arr[right] = hv;
     }
 }
