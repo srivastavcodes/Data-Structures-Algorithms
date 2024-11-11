840e88973b36a7532e5d4dfb47aaf0538fd837ac
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-11 16:15:53 +0530

    code studio questions with self-explanatory names.

---
+++ Step 3/src/Easy/LinearSearch.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 1, 54, 3, 9, 4};
+        int n = 54;
+        int got = linearSearch(arr, n, arr.length);
+        System.out.println(got);
+    }
+
+    private static int linearSearch(int[] arr, int num, int len) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Step 3/src/Easy/MoveZeroToEnd.java
@@ -0,0 +1,46 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class MoveZeroToEnd {
+    public static void main(String[] args) {
+
+        int[] arr = {0, 0, 0, 1};
+        int[] res = moveZeros(arr, arr.length - 1);
+        System.out.println(Arrays.toString(res));
+    }
+
+    private static int[] moveZeros(int[] arr, int len) {
+        int right = -1;
+
+        for (int i = 0; i <= len; i++) {
+            if (arr[i] == 0) {
+                right = arr[i];
+                break;
+            }
+        }
+        // if no zero found.
+        if (right == -1) return arr;
+
+        for (int i = 0; i <= len; i++) {
+            if (arr[i] != 0) {
+                swapElements(arr, i, right);
+                right += 1;
+            }
+        }
+        return arr;
+    }
+
+    private static void swapElements(int[] arr, int left, int right) {
+        int hvar = arr[left];
+        arr[left] = arr[right];
+        arr[right] = hvar;
+    }
+}
+
+
+
+
+
+
+
