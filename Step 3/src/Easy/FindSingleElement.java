9374b53b857d96e76ba25372bf26916e94914d5e
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-12 09:53:22 +0530

    code studio questions with self-explanatory names.

---
+++ Step 3/src/Easy/FindSingleElement.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class FindSingleElement {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 3, 3, 4, 4};
+        int res = getSingleElement(arr);
+        System.out.print(res);
+    }
+
+    public static int getSingleElement(int[] arr) {
+        int xor = 0;
+        for (int i : arr) xor = xor ^ i;
+        return xor;
+    }
+}
