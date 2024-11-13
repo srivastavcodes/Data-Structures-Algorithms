df5e1d7424de5d32b8d8e9ef6749bca27ca1e372
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-14 02:45:53 +0530

    Sort array of 0's, 1's, and 2's

---
+++ Step 3/src/medium/DutchNationalFlagAlgoImpl.java
@@ -0,0 +1,38 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DutchNationalFlagAlgoImpl {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(2, 2, 2, 2, 0, 0, 1, 0);
+        ArrayList<Integer> arrList = new ArrayList<>(list);
+        sortArray(arrList, arrList.size());
+    }
+
+    public static void sortArray(ArrayList<Integer> arrList, int size) {
+        int low = 0, mid = 0, high = size - 1;
+
+        while (mid <= high) {
+            if (arrList.get(mid) == 0) swapElements(arrList, low++, mid++);
+            else if (arrList.get(mid) == 1) mid += 1;
+            else swapElements(arrList, mid, high--);
+        }
+        System.out.println(arrList);
+    }
+
+    private static void swapElements(ArrayList<Integer> arrList, int left, int right) {
+        int hv = arrList.get(left);
+        arrList.set(left, arrList.get(right));
+        arrList.set(right, hv);
+    }
+}
+
+
+
+
+
+
+
