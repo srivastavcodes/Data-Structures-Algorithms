a30983d26ac2af921cfe0519e589a27bea728e2b
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-13 13:29:57 +0530

    longest subarray -10ˆ6 <= 10ˆ6

---
+++ Step 3/src/Easy/LongestSubArrInteger.java
@@ -0,0 +1,38 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubArrInteger {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 0, 1};
+        int k = 4;
+        int subArrLen = getLongestSubArray(arr, k);
+        System.out.print(subArrLen);
+    }
+
+    public static int getLongestSubArray(int[] arr, int k) {
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0, maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            if (sum == k) maxLength = Math.max(maxLength, i + 1);
+
+            int isPresent = sum - k;
+            if (preSumMap.containsKey(isPresent)) {
+                maxLength = Math.max(maxLength, i - preSumMap.get(isPresent));
+            }
+            if (!preSumMap.containsKey(sum)) preSumMap.put(sum, i);
+        }
+        return maxLength;
+    }
+}
+
+
+
+
+
+
+
