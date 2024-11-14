4460dd90e05ce4b5a1a4b40ce6837c095cc93faa
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-14 20:11:21 +0530

    majority element "better approach". Optimal in the next commit.

---
+++ Step 3/src/medium/MajorityElement.java
@@ -0,0 +1,26 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+
+        int[] vals = {2, 2, 1, 3, 3, 2, 2};
+        int majorityE = majorityElement(vals);
+        System.out.print(majorityE);
+    }
+
+    public static int majorityElement(int[] vals) {
+        HashMap<Integer, Integer> intMap = new HashMap<>();
+        for (int i : vals) {
+            intMap.put(i, intMap.getOrDefault(i, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> val : intMap.entrySet()) {
+            if (val.getValue() > intMap.size() / 2) {
+                return val.getKey();
+            }
+        }
+        return -1;
+    }
+}
