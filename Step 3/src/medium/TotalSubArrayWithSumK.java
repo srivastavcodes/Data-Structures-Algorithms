becb1aa9b2b581419b120e8befe4133a81d7e874
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-11-20 18:17:34 +0530

    total sub array with sum k

---
+++ Step 3/src/medium/TotalSubArrayWithSumK.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TotalSubArrayWithSumK {
+    public static void main(String[] args) {
+
+        int[] vals = {3, 1, 2, 4};
+        int k = 6;
+        int result = getSubarraysWithSumK(vals, k);
+        System.out.print(result);
+    }
+
+    public static int getSubarraysWithSumK(int[] vals, int k) {
+        Map<Integer, Integer> integerMap = new HashMap<>();
+        integerMap.put(0, 1);
+        int prefixSum = 0, count = 0;
+
+        for (int i = 0; i < vals.length; i++) {
+            prefixSum += vals[i];
+            int left = prefixSum - k;
+            if (integerMap.get(left) != null) count += integerMap.get(left);
+            integerMap.put(prefixSum, integerMap.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
