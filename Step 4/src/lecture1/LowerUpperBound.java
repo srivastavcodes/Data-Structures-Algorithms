c44c01685ac59a2b6f0c952b794dc77800ebd5d2
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-03 17:50:10 +0530

    floor and ceil

--- Step 4/src/lecture1/LowerUpperBound.java
+++ Step 4/src/lecture1/LowerUpperBound.java
@@ -1,14 +1,17 @@
 package lecture1;
 
+import java.util.Arrays;
+
 class LowerUpperBound {
     public static void main(String[] args) {
 
-        int[] vals = {1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9};
+        int[] vals = {3, 4, 4, 7, 8, 10};
         int len = vals.length;
-        int k = 2;
+        int k = 8;
         System.out.print(lowerBound(vals, len, k));
         System.out.print("\n" + upperBound(vals, len, k));
-        System.out.print(searchInsert(vals, k));
+        System.out.print("\n" + searchInsert(vals, k));
+        System.out.print("\n" + Arrays.toString(getFloorAndCeil(vals, len, k)));
     }
 
     private static int lowerBound(int[] vals, int len, int k) {
@@ -52,4 +55,32 @@ class LowerUpperBound {
         }
         return result;
     }
+
+    private static int[] getFloorAndCeil(int[] vals, int len, int x) {
+        int low = 0, high = len - 1;
+        int[] result = {-1, -1};
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (vals[mid] <= x) {
+                result[0] = vals[mid];
+                low = mid + 1;
+            }
+            if (vals[mid] >= x) {
+                result[1] = vals[mid];
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
 }
+
+
+
+
+
+
+
+
+
