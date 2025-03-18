c3b6e9e3d7b54465e54db0fbd8a6bc62d8f693bc
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-18 16:36:20 +0530

    deletion

--- LinkedList/src/lecture1/IntroToLL.java
+++ LinkedList/src/lecture1/IntroToLL.java
@@ -3,13 +3,12 @@ package lecture1;
 public class IntroToLL {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        Node head = constructLL(arr);
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data+ " ");
-            temp = temp.next;
-        }
-        System.out.print("\n" + length(head));
+        Node node = constructLL(arr);
+        node = removeKthNode(node, 8);
+        printList(node);
+        System.out.println();
+        node = removeElmnt(node, 2);
+        printList(node);
     }
 
     private static Node constructLL(int[] arr) {
@@ -40,6 +39,62 @@ public class IntroToLL {
             temp = temp.next;
         }
         return 0;
+    }
+
+    private static Node removesHead(Node node) {
+        if (node == null) return null;
+        return node.next;
+    }
+
+    private static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    private static Node removeTail(Node node) {
+        if (node == null || node.next.next == null) return null;
+        Node temp = node;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return node;
+    }
+
+    private static Node removeKthNode(Node node, int k) {
+        if (node == null) return null;
+        if (k == 1) return node.next;
+        int count = 0;
+        Node temp = node;
+        Node prev = null;
+        while (temp != null) {
+            count++;
+            if (count == k) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return node;
+    }
+
+    private static Node removeElmnt(Node node, int k) {
+        if (node == null) return null;
+        if (node.data == k) return node.next;
+        Node temp = node;
+        Node prev = null;
+        while (temp != null) {
+            if (temp.data == k) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return node;
     }
 }
 
