568a1daf9df890c35b8bf1ce2b18b2cfafb67664
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-04-02 14:29:44 +0530

    deletion of node

--- LinkedList/src/lecture2/Main.java
+++ LinkedList/src/lecture2/Main.java
@@ -1,14 +1,21 @@
 package lecture2;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+      public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6, 7};
 
-        IntroToDLL dll = new IntroToDLL();
-        Node head = dll.convertTODLL(arr);
-        dll.printDLL(head);
-        System.out.println();
-        Node afterDeletion = dll.deleteKthNode(head, 0);
-        dll.printDLL(afterDeletion);
-    }
+            IntroToDLL dll = new IntroToDLL();
+            Node head = dll.convertTODLL(arr);
+            dll.printDLL(head);
+
+            System.out.println();
+
+            Node afterDeletion = dll.removeKthNode(head, 0);
+            dll.printDLL(afterDeletion);
+
+            System.out.println();
+
+            dll.deleteNode(afterDeletion.next);
+            dll.printDLL(afterDeletion);
+      }
 }
--- LinkedList/src/lecture2/Node.java
+++ LinkedList/src/lecture2/Node.java
@@ -1,28 +1,28 @@
 package lecture2;
 
 public class Node {
-    public Node prev;
-    public int value;
-    public Node next;
+      public Node prev;
+      public int value;
+      public Node next;
 
-    public Node() {
-    }
+      public Node() {
+      }
 
-    public Node(int value) {
-        this.prev = null;
-        this.value = value;
-        this.next = null;
-    }
+      public Node(int value) {
+            this.prev = null;
+            this.value = value;
+            this.next = null;
+      }
 
-    public Node(Node prev, int value) {
-        this.prev = prev;
-        this.value = value;
-        this.next = null;
-    }
+      public Node(Node prev, int value) {
+            this.prev = prev;
+            this.value = value;
+            this.next = null;
+      }
 
-    public Node(Node prev, Node next, int value) {
-        this.prev = prev;
-        this.value = value;
-        this.next = next;
-    }
+      public Node(Node prev, Node next, int value) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+      }
 }
