4c73b6ca5366661c8fb3e0bd9ce46044ab333dd1
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-04-23 22:02:59 +0530

    permutations of a string

--- Recursion/SubsetSequenceString/main.go
+++ Recursion/SubsetSequenceString/main.go
@@ -3,17 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	skip('c', "", "baccdah")
-	result := skipReturn('a', "baaccdah")
-	skippedString := skipString("apple", "badappleiate")
-	fmt.Println(result)
-	fmt.Println(skippedString)
-
-	var resList []string
-	subseq("", "123", &resList)
-	fmt.Println(resList)
-
-	var resList2 []string
-	resList2 = subseq2("", "123")
-	fmt.Println(resList2)
+	perm := permutations("", "abc")
+	fmt.Println(perm)
 }
