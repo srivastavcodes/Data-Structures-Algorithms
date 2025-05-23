4e6121c53328eada9a0c89f54190ad3a7cc214e3
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-05-23 11:45:49 +0530

    backtracking medium

--- NeetCode/backtracking/backtracking.go
+++ NeetCode/backtracking/backtracking.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	digits := []int{1, 2, 3}
-	output := subsets(digits)
-	fmt.Println(output)
+	digits := []int{4, 4, 4, 1, 4}
+
+	// output := subsets(digits)
+	// fmt.Println(output)
+
+	// comb := combinationSum2(digits, 8)
+	// fmt.Println(comb)
+
+	// perm := permute(digits)
+	// fmt.Println(perm)
+
+	subsetII := subsetsWithDup(digits)
+	fmt.Println(subsetII)
 }
