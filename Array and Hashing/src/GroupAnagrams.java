ec0e044045db36f5d7844e5b6cf540fe6cbd91fc
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-19 22:48:22 +0530

    group anagrams

--- Array and Hashing/src/GroupAnagrams.java
+++ Array and Hashing/src/GroupAnagrams.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class GroupAnagrams {
 	public static void main(String[] args) {
+		String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
+		List<List<String>> result = groupAnagrams(str);
+		System.out.println(result);
+	}
 
+	public static List<List<String>> groupAnagrams(String[] args) {
+		Map<String, List<String>> result = new HashMap<>();
+		for (String str : args) {
+			int[] count = new int[26];
+			for (char c : str.toCharArray()) {
+				count[c - 'a']++;
+			}
+			String key = Arrays.toString(count);
+			result.putIfAbsent(key, new ArrayList<>());
+			result.get(key).add(str);
+		}
+		return new ArrayList<>(result.values());
 	}
 }
